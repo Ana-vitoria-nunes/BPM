31d4fb2906146bd81bc5d775cf7e35267c1f6a4d
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-09-19 15:00:59 -0300

    Concluido segunda etapa

---
+++ src/main/java/org/example/Calcular.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calcular {
+    public int calcularFatorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calcularFatorial(n - 1);
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Calcular calcular=new Calcular();
+
+        boolean entradaValida = false;
+        int numero;
+        do {
+            try {
+                System.out.print("Digite um número inteiro entre 0 e 12: ");
+                String input = scanner.next();
+                numero = Integer.parseInt(input);
+
+                if (numero >= 0 && numero <= 12) {
+                    entradaValida = true;
+                    System.out.println(numero + "! = " + calcular.calcularFatorial(numero));
+                } else {
+                    System.out.println("O número deve estar entre 0 e 12.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida. Digite um número inteiro válido.");
+            }
+        } while (!entradaValida);
     }
 }
